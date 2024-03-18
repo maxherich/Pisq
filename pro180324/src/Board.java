b42fe4de19d6533cba8cfef820db9c21840101eb
simon9834 <hloska@spsejecna.cz>
2024-03-18 11:06:18 +0100

    updated board (not working)

--- pro180324/src/Board.java
+++ pro180324/src/Board.java
@@ -2,26 +2,39 @@ public class Board {
 
     private int size;
 
-
     public void setBoardSize(int size){
         this.size = size;
     }
     // creates a board for a graphical output
     public String createBoard(){
-        String board = "";
+        String board = "    ";
         String myChar = "‖    ";
-        String myMidChar = "  ⩴ ";
+        String myMidChar = "    ⩴ ";
         for(int i = 0; i < size; i++){
             myChar += "‖    ";
         }
         for(int i = 1; i < size; i++){
             myMidChar += "  ⩴ ";
+            if((i%4)==0){
+                myMidChar += "   ⩴ ";
+                i = 7;
+            }
         }
-        for(int i = 0; i < size;i++){
+        for(int i = 0; i < size; i++){
+            board += i + "    ";
+            if((i%3)==0){
+                board += i + "     ";
+                i=6;
+            }
+        }
+        board += "\n";
+        for(int i = 0; i < size; i++){
             board += myMidChar + "\n";
-            board += myChar + "\n";
+            board += i + " " + myChar + "\n";
         }
         return board + myMidChar;
     }
+
+   // public String
 
 }
