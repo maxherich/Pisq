b4933565fdf6a11e96093801b9505ae616358f8d
simon9834 <hloska@spsejecna.cz>
2024-03-18 10:39:01 +0100

    - added method create board

--- pro180324/src/Board.java
+++ pro180324/src/Board.java
@@ -4,7 +4,24 @@ public class Board {
 
 
     public void setBoardSize(int size){
-        size = this.size;
+        this.size = size;
+    }
+    // creates a board for a graphical output
+    public String createBoard(){
+        String board = "";
+        String myChar = "‖    ";
+        String myMidChar = "  ⩴ ";
+        for(int i = 0; i < size; i++){
+            myChar += "‖    ";
+        }
+        for(int i = 1; i < size; i++){
+            myMidChar += "  ⩴ ";
+        }
+        for(int i = 0; i < size;i++){
+            board += myMidChar + "\n";
+            board += myChar + "\n";
+        }
+        return board + myMidChar;
     }
 
 }
--- pro180324/src/Main.java
+++ pro180324/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Board board = new Board();
+        board.setBoardSize(4);
+        System.out.println(board.createBoard());
     }
 }
