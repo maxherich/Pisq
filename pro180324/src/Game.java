a468c756ef87390f7ab49540e0896572f1db3984
herich <max.herich@gmail.com>
2024-03-18 11:06:45 +0100

    Adding playersTurn method

--- pro180324/src/Game.java
+++ pro180324/src/Game.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     ArrayList <Player> players = new ArrayList<>();
-    private Player player1;
-    private Player player2;
+    private Player player1 = new Player();
+    private Player player2 = new Player();
     private int playersTurn;
     private Player winner;
+    Scanner scanner = new Scanner(System.in);
 
     public Game() {
         player1.setName("Hynek");
@@ -15,11 +17,30 @@ public class Game {
         players.add(player1);
         players.add(player2);
         playersTurn = 0;
+        String playersAnswer;
+        Board board = new Board();
+        board.setBoardSize(3);
+        System.out.println(board.createBoard());
+        System.out.println(players.get(playersTurn).getName() + " choose a box you want to put your tag (example: 1 2)");
+        playersAnswer = scanner.next();
+        //board.addTag
+        changePlayersTurn(playersTurn);
 
-        System.out.println(players.get(playersTurn).getName() + " choose a box you want to put your tag");
 
 
 
 
+
+
+
+    }
+
+    public int changePlayersTurn (int playersTurn){
+        if (playersTurn == 0){
+            playersTurn = playersTurn++;
+        }else if (playersTurn == 1){
+            playersTurn = playersTurn--;
+        }
+        return playersTurn;
     }
 }
